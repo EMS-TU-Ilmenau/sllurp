a6329c99b9f3cd60fdc789ee3a60a44e9d29f655
Niklas Beuster <niklas.beuster@tu-ilmenau.de>
2018-05-18 13:36:57 +0200

    Increased version to reflect change

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def read(filename):
 
 setup(
     name='sllurp',
-    version='0.3.0',
+    version='0.3.1',
     description='RFID reader control library',
     long_description=read('README.rst'),
     author='Ben Ransford',
