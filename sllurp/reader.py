3c86677bd9996c3533cfa008b76828cd841cb8e2
Niklas Beuster <niklas.beuster@tu-ilmenau.de>
2019-10-30 18:02:33 +0100

    added Kathrein ARU2400 reader

--- sllurp/reader.py
+++ sllurp/reader.py
@@ -5,26 +5,6 @@ import threading # for making live tag reports non-blocking
 Classes for specific reader implementations
 '''
 
-def nearestIndex(arr, val):
-	'''
-	Searches for a value in an array and return index for best match
-	:param arr: array with values (int / float)
-	:param val: int or float value to search for in the array
-	:returns: index for best match of value in array
-	'''
-	smallestDiff = 2**64-1
-	iMatch = 0
-	for iArr, vArr in enumerate(arr):
-		# compare array value and val
-		diff = abs(vArr-val)
-		if diff < smallestDiff:
-			# found smaller difference, so remember
-			iMatch = iArr
-			smallestDiff = diff
-	
-	return iMatch
-			
-
 class R420_EU(LLRPClient):
 	def __init__(self, ip='192.168.4.2', includeEPCs=[], excludeEPCs=[], *args, **kwargs):
 		''':param ip: IP address of the reader
@@ -63,17 +43,36 @@ class R420_EU(LLRPClient):
 		self.startConnection()
 		print('Connected to reader')
 	
+	def nearestIndex(self, arr, val):
+		'''
+		Searches for a value in an array and return index for best match
+		:param arr: array with values (int / float)
+		:param val: int or float value to search for in the array
+		:returns: index for best match of value in array
+		'''
+		smallestDiff = 2**64-1
+		iMatch = 0
+		for iArr, vArr in enumerate(arr):
+			# compare array value and val
+			diff = abs(vArr-val)
+			if diff < smallestDiff:
+				# found smaller difference, so remember
+				iMatch = iArr
+				smallestDiff = diff
+
+		return iMatch
+	
 	def getPowerIndex(self, powDBm):
 		'''search nearest matching power in table
 		:param powDBm: power in dBm
 		:returns: table index'''
-		return nearestIndex(self.power_table, powDBm)+1
+		return self.nearestIndex(self.power_table, powDBm)+1
 	
 	def getChannelIndex(self, freqMHz):
 		'''search nearest matching channel in table
 		:param freqMHz: frequency in MHz
 		:returns: table index'''
-		return nearestIndex(self.freq_table, freqMHz)+1
+		return self.nearestIndex(self.freq_table, freqMHz)+1
 	
 	def filterTags(self, trp):
 		'''Filters tags based on the EPC filters specified on construction
@@ -167,8 +166,6 @@ class R420_EU(LLRPClient):
 		self.session = session
 		self.population = population
 		self.antennas = antennas
-		# check settings against capabilities
-		self.parseCapabilities(self.capabilities)
 		
 		# we want to get informed when tags are reported
 		self._liveReport = reportCallback
@@ -222,3 +219,112 @@ class R420_EU(LLRPClient):
 				epcs.append(epc)
 		
 		return epcs
+
+
+class ARU2400(R420_EU):
+	def __init__(self, ip='192.168.4.2', includeEPCs=[], excludeEPCs=[], *args, **kwargs):
+		''':param ip: IP address of the reader
+		:param includeEPCs: string or list of strings containing EPCs to look for during inventory.
+			Other tags will not be reported when used.
+		:param excludeEPCs: string or list of strings containing EPCs to ignore during inventory.
+			Tags with these EPCs will not be reported when used.
+		'''
+		# epc filters
+		self.includeEPCs = includeEPCs
+		self.excludeEPCs = excludeEPCs
+		
+		# init common llrp stuff
+		LLRPClient.__init__(self, ip, *args, **kwargs)
+		
+		# select what data we want to get from the reader
+		self.report_selection = {
+			'EnableROSpecID': False,
+			'EnableSpecIndex': False,
+			'EnableInventoryParameterSpecID': False,
+			'EnableAntennaID': True,
+			'EnableChannelIndex': True,
+			'EnablePeakRRSI': True,
+			'EnableFirstSeenTimestamp': True,
+			'EnableLastSeenTimestamp': True,
+			'EnableTagSeenCount': True,
+			'EnableAccessSpecID': False}
+		
+		# connect to reader
+		self.startConnection()
+		print('Connected to reader')
+	
+	def detectTags(self, powerDBm=27., freqMHz=866.9, duration=0.5, mode=12, session=2, population=1, antennas=(1,), rounds=1):
+		'''starts the readers inventoring process and return the found tags.
+		
+		:param duration: gives the reader that much time in seconds to find tags
+		:param powerDBm: tx power in dBm. 
+			Valid values are 10...31.5 in 0.25 steps
+		:param freqMHz: frequency band in MHz. 
+			Valid values for EU are 865.7, 866.3, 866.9, 867.5
+		:param mode: preset mode identifier which defines tari, miller, etc. 
+			Valid values are 1002, 1000, 5, 3, 2, 1, 0
+		:param session: controls tag muting behaviour
+		:param population: number of tags estimated in the readers scope
+		:antennas: tuple of antenna ports to use for inventory.
+			Set to (0,) to scan automatically over all
+		:param rounds: number of tag reports until stopping inventoring
+		:returns: list of detected tags with their meta informations
+		'''
+		# update settings
+		self.report_interval = duration
+		self.power = self.getPowerIndex(powerDBm)
+		self.channel = self.getChannelIndex(freqMHz)
+		self.mode_identifier = mode
+		self.session = session
+		self.population = population
+		self.antennas = antennas
+		
+		# prepare inventory
+		self.rounds = rounds
+		self.round = 0
+		self.detectedTags = []
+		# we want to get informed when tags are reported
+		self.addMsgCallback('RO_ACCESS_REPORT', self.foundTags)
+		
+		# start inventory
+		self.startInventory()
+		# wait for tagreport(s)
+		while self.round < rounds:
+			self.readLLRPMessage('RO_ACCESS_REPORT')
+		
+		# don't need more reports
+		self.removeMsgCallback('RO_ACCESS_REPORT', self.foundTags)
+		# stop inventoring
+		self.stopPolitely()
+		
+		# return results
+		if rounds == 1:
+			return self.detectedTags[0]
+		else:
+			return self.detectedTags
+	
+	def startLiveReports(self, reportCallback, powerDBm=27., freqMHz=866.9, duration=1.0, mode=12, session=2, population=1, antennas=(1,)):
+		'''starts the readers inventoring process and 
+		reports tagreports periodically through a callback function.
+		
+		:param reportCallback: call back function which is called every 
+			"duration" seconds with the tagreport as argument
+		the other parameters are the same as in "detectTags"
+		'''
+		# update settings
+		self.report_interval = duration
+		self.power = self.getPowerIndex(powerDBm)
+		self.channel = self.getChannelIndex(freqMHz)
+		self.mode_identifier = mode
+		self.session = session
+		self.population = population
+		self.antennas = antennas
+		
+		# we want to get informed when tags are reported
+		self._liveReport = reportCallback
+		self.addMsgCallback('RO_ACCESS_REPORT', self._foundTagsLive)
+		
+		# continue non-blocking
+		self._liveStop = threading.Event()
+		self._liveThread = threading.Thread(target=self._liveInventory, args=(self._liveStop,))
+		self._liveThread.start()
